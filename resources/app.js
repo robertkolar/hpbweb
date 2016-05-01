b8175912b167145792683085decd40dcc7284347
Robert Kolar <robert.kolar@highpowerbear.com>
2016-05-01 15:57:13 -0400

    Reformatted liquid tags to prevent producing excessive blank lines, refactored app.js, added google analytics js code.

--- resources/app.js
+++ resources/app.js
@@ -2,42 +2,40 @@
  * Created by robertk on 4/26/2016.
  */
 $(document).ready(function() {
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
-
-    $('.hpb-button').click(function () {
-        Hpb.swapImage($(this).attr('id'));
-    });
-    Hpb.initCarousel();
-    Hpb.scrollUp();
+    Hpb.initSwapChartImage();
+    Hpb.initFrontPageCarousel();
+    Hpb.initScrollUp();
+    Hpb.initEkkoLightbox();
+    Hpb.initGoogleAnalytics();
 });
 
 var Hpb = (function() {
     var my = {};
-    
-    my.swapImage = function(buttonId) {
-        var els = buttonId.split('-'),
-            symbol = els[1],
-            ymd = els[2],
-            interval = els[3],
-            suffix = els[4];
 
-        var chartLinkId = 'chartlink-' + symbol + '-' + ymd;
-        var chartImageId = 'chartimage-' + symbol + '-' + ymd;
-        var imgSrc = '/charts/' + ymd.substr(0, 2) + '/' + ymd.substr(2, 2) + '/' + ymd.substr(4, 2) + '/' + symbol + '_' + ymd + '_' + interval + '.' + suffix;
+    my.initSwapChartImage = function() {
+        $('.hpb-button').click(function () {
+            var buttonId = $(this).attr('id'),
+                els = buttonId.split('-'),
+                symbol = els[1],
+                ymd = els[2],
+                interval = els[3],
+                suffix = els[4];
 
-        $('#' + chartLinkId).attr('href', imgSrc);
-        $('#' + chartImageId).on('load', function () {
-            console.log('loaded');
-            $('#' + buttonId).addClass('active').siblings().removeClass('active');
-            $('#' + chartImageId).off('load');
-        }).attr('src', imgSrc);
-        return true;
+            var chartLinkId = 'chartlink-' + symbol + '-' + ymd;
+            var chartImageId = 'chartimage-' + symbol + '-' + ymd;
+            var imgSrc = '/charts/' + ymd.substr(0, 2) + '/' + ymd.substr(2, 2) + '/' + ymd.substr(4, 2) + '/' + symbol + '_' + ymd + '_' + interval + '.' + suffix;
+
+            $('#' + chartLinkId).attr('href', imgSrc);
+            $('#' + chartImageId).on('load', function () {
+                console.log('loaded');
+                $('#' + buttonId).addClass('active').siblings().removeClass('active');
+                $('#' + chartImageId).off('load');
+            }).attr('src', imgSrc);
+            return true;
+        });
     };
     
-    my.initCarousel = function() {
+    my.initFrontPageCarousel = function() {
         // Activate Carousel
         $("#front-page-carousel").carousel();
 
@@ -48,7 +46,6 @@ var Hpb = (function() {
         $(".item2").click(function() {
             $("#front-page-carousel").carousel(1);
         });
-
         // Enable Carousel Controls
         $(".left").click(function() {
             $("#front-page-carousel").carousel("prev");
@@ -58,7 +55,7 @@ var Hpb = (function() {
         });
     };
     
-    my.scrollUp = function() {
+    my.initScrollUp = function() {
         $(window).scroll(function () {
             if ($(this).scrollTop() > 100) {
                 $('.hpb-scrollup').fadeIn();
@@ -74,5 +71,23 @@ var Hpb = (function() {
             return false;
         });
     };
+
+    my.initEkkoLightbox = function() {
+        $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });
+    };
+
+    my.initGoogleAnalytics = function() {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-11271802-3', 'auto');
+        ga('send', 'pageview');
+    };
+
     return my;
 }());
